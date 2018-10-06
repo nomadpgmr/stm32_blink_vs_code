eeefdf312303877d0d74b34dac5b0c825f38ca55
unknown <rogerw@vendnovation.com>
2018-10-06 16:14:23 -0700

    Initial commit of code and files required to build. Does not include compiler and OpenOCD, which need to be installed seperately

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# STM32 + Visual Studio Code
+Building and debugging the STM32 IoT B-L475E-IOT01A with Visual Studio Code
+
+
+## Requirements
+
+* Install OpenOCD
+* Install Compiler
+
---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include "mbed.h"
+
+DigitalOut led1(LED1);
+DigitalOut led4_blue_led(LED4);
+// Serial pc(USBTX, USBRX);
+
+class MyLed
+{
+   public:
+
+   MyLed()
+   {
+   }
+   ~MyLed()
+   {
+
+   }
+
+   void blink(float delay)
+   {
+      led1 = 1;
+      wait(delay);
+      led1 = 0;
+      wait(delay);
+   }
+};
+
+int main()
+{
+
+   led4_blue_led = 1; // 0 == on
+   
+   MyLed *myled = new MyLed();
+   while (1)
+   {
+      myled->blink(0.2);
+      myled->blink(0.2);
+      myled->blink(0.2);
+      myled->blink(0.2);
+      myled->blink(0.2);
+      wait(1.0);
+      myled->blink(0.2);
+      wait(1.0);
+      // pc.printf("Hello World\r\n");
+      // printf("Hello World\n");
+   };
+
+   free(myled);
+
+}
